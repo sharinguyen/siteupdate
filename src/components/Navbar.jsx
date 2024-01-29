c523e14cdbd708999d6eb62c554620dec27cbb90
Shari <96545440+sharinguyen@users.noreply.github.com>
2024-01-28 22:37:18 -0800

    update footer, navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import React, {useState} from "react";
 import Logo from "../assets/GEG-white.png";
 import {Link}  from 'react-router-dom';
 import '../styles/Navbar.css';
+import Dropdownlocations from "../helpers/DropdownLocations.jsx";
 
 
+//Icons install with npm install @mui/material @emotion/react @emotion/styled
+//and npm install @mui/icons-material
 import AccountBox from '@mui/icons-material/AccountBox';
 import ShoppingCart from '@mui/icons-material/ShoppingCart';
 import ReorderIcon from'@mui/icons-material/Reorder';
@@ -17,38 +20,48 @@ function Navbar() {
         setOpenLinks(!openLinks);
     };
 
+    {/*dropdown for locations */}
+
 return (
     /*navbar and link */
     <div className="navbar" >
         <div className="leftSide" id={openLinks ? "open": "close"}>
+            {/*logo icon can traceback to home page */}
+        <Link to ='./pages/Home'>
         <img src={Logo} alt="Great Escape Games Logo"/>
+        </Link>
         <div className="hiddenLinks">
-             <Link to ='./pages/Home'>Home</Link>
-            <Link to ='./pages/Store'>Store</Link>
-            <Link to ='./pages/Locations'>Locations</Link>
+            <Link to ='./pages/Home'>Home</Link>
+            <Link to ='./pages/Product'>Product</Link>
+
+             {/* Use the Dropdown locations */}
+             < Dropdownlocations />
+
             <Link to ='./pages/PrivateRooms'>Private Rooms</Link>
             <Link to ='./pages/About'>About</Link>
             <Link to ='./pages/Contact'>Contact</Link>
         </div>
 
         </div>
-     
+     {/*pages link to respective spots. uses import page on top and link images */}
         <div className="rightSide">
             <Link to ='./pages/Home'>Home</Link>
-            <Link to ='./pages/Store'>Store</Link>
-            <Link to ='./pages/Locations'>Locations</Link>
+            <Link to ='./pages/Product'>Product</Link>
+
+              {/* Use the Dropdown locations*/}
+              < Dropdownlocations />
             <Link to ='./pages/PrivateRooms'>Private Rooms</Link>
             <Link to ='./pages/About'>About</Link>
             <Link to ='./pages/Contact'>Contact</Link>
-
-            <div className = 'NavIcon'>
             <Link to='./pages/Login'>
+
           <AccountBox alt="account icon" />
         </Link>
         <Link to='./pages/About'>
            <ShoppingCart  alt="shopping cart"/>
            </Link>
-           </div>
+           
+{/*When screen is small itll show the reorder button that is toggle on and off, uses hiddenlink className */}
            <button onClick={toggleNavbar}>
            <ReorderIcon />
            </button>
